c31e64ac91817250f19241b653c079cda27a231f
sh4aadman <sh4adman@gmail.com>
2026-01-17 18:32:06 +0600

    17-01-26

--- Searching Problems/FirstAndLastPosition.js
+++ Searching Problems/FirstAndLastPosition.js
@@ -1,19 +1,29 @@
 // Leetcode - Binary Search - Problem 34
 
 function firstAndLastPosition(nums, target) {
-    var left = 0;
-    var right = nums.length - 1;
-    var start = -1;
-    var end = -1;
-    while (left <= right) {
-        let middle = Math.floor((left + right) / 2);
-        if (nums[middle === target]) start = middle;
-        else if (nums[middle] < target) left = middle + 1;
-        else right = middle - 1;
-        end = right;
-    }
+  var left = binarySearch(nums, target, true);
+  var right = binarySearch(nums, target, false);
+  return [left, right];
 
-    return [start, end];
+  function binarySearch(nums, target, leftBias) {
+    var start = 0;
+    var end = nums.length - 1;
+    var i = -1;
+    while (start <= end) {
+      var middle = Math.floor((start + end) / 2);
+      if (target < nums[middle]) end = middle - 1;
+      else if (target > nums[middle]) start = middle + 1;
+      else {
+        i = middle;
+        if (leftBias) end = middle - 1;
+        else start = middle + 1;
+      }
+    }
+    return i;
+  }
 }
 
-console.log(firstAndLastPosition([5,7,7,8,8,10], 8))
+console.log(firstAndLastPosition([5, 7, 7, 8, 8, 10], 8));
+
+// Time Complexity - O(n)
+// Space Complexity - O(1)
