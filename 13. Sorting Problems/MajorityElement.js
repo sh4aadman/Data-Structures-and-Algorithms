4f24457e10562158d2e94fab6da68d93450b9fb3
sh4aadman <sh4adman@gmail.com>
2026-01-22 23:40:06 +0600

    22-01-26

--- 13. Sorting Problems/MajorityElement.js
+++ 13. Sorting Problems/MajorityElement.js
@@ -1,10 +1,33 @@
 // Leetcode - Sorting - Problem 169
 
 function majorityElement(nums) {
-  var sorted = nums.sort();
-    for (var i = 0; i < sorted.length; i++) {
-        let count = 
-    }
+  var result = 0;
+  var majority = 0;
+  for (let n of nums) {
+    if (majority === 0) result = n;
+    majority += n === result ? 1 : -1;
+  }
+
+  return result;
 }
 
+// Space Complexity - O(n)
+// function majorityElement(nums) {
+//     var hash = {};
+//     var result = 0;
+//     var majority = 0;
+//     for (let n of nums) {
+//         hash[n] = 1 + (hash[n] || 0);
+//         if (hash[n] > majority) {
+//             result = n;
+//             majority = hash[n];
+//         }
+//     }
+//     return result;
+//  }
+
+
 console.log(majorityElement([2, 2, 1, 1, 1, 2, 2]));
+
+// Time Complexity - O(n)
+// Space Complexity - O(1)
