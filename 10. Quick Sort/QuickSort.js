1b55ca3eeec792086bea418d1028e809f3c0e72e
sh4aadman <sh4adman@gmail.com>
2026-01-27 20:43:59 +0600

    27-01-26

--- 10. Quick Sort/QuickSort.js
+++ 10. Quick Sort/QuickSort.js
@@ -1,2 +1,36 @@
 // Quick Sort Algorithm
 
+function pivot(arr, start = 0, end = arr.length - 1) {
+  function swap(array, x, y) {
+    var temp = array[x];
+    array[x] = array[y];
+    array[y] = temp;
+  }
+
+  var pivot = arr[start];
+  var swapIdx = start;
+  for (var i = start + 1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
+    }
+  }
+  swap(arr, start, swapIdx);
+
+  return swapIdx;
+}
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    quickSort(arr, left, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+
+// console.log(pivot([4, 3, 7, 1, 2, 9]));
+console.log(quickSort([4, 3, 7, 1, 2, 9]));
+
+// Time Complexity - O(nlogn)
+// Space Complexity - O(logn)
---
+++ 10. Quick Sort/QuickSortEx.js
@@ -0,0 +1,31 @@
+function strLength(a, b) {
+  return a.length - b.length
+}
+
+function pivot(arr, comparator, start = 0, end = arr.length - 1) {
+  function swap(array, x, y) {
+    var temp = array[x];
+    array[x] = array[y];
+    array[y] = temp;
+  }
+
+  var swapIdx = start;
+  var pivot = arr[start];
+
+  if (typeof comparator !== "function") {
+    comparator = (a, b) => a - b;
+  }
+
+  for (let i = start + 1; i < arr.length; i++) {
+    if (comparator(pivot, arr[i]) > 0) {
+      swapIdx++;
+      swap(arr, i, swapIdx);
+    }
+  }
+  swap(arr, start, swapIdx);
+
+  return swapIdx;
+}
+
+// console.log(pivot([8, 4, 2, 5, 0, 10, 11, 12, 13, 16]));
+console.log(pivot(["LilBub", "Garfield", "Heathcliff", "Blue", "Grumpy"], strLength));
