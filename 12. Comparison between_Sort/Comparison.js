b9c9e56ce2d13f5efd629d5c852961da01594af7
sh4aadman <sh4adman@gmail.com>
2026-01-29 04:33:39 +0600

    29-01-26

--- 12. Comparison between_Sort/Comparison.js
+++ 12. Comparison between_Sort/Comparison.js
@@ -1,7 +1,11 @@
 // Comparison between Bubble Sort, Selection Sort, and Insertion Sort
 
-// For certain cases, like for a nearly sorted array,
+// For certain cases, like for a nearly sorted array ->
 
 // Bubble Sort and Insertion Sort works faster.
 
 // Selection Sort works slower, though all of them has O(n ^ 2) time complexity and O(1) space complexity.
+
+// Merge Sort and Quick Sort has O(n logn) time complexity along with the space complexity of O(n).
+
+// Radix Sort has time complexity of O(n * k) and space complexity of O(n + k) - usually works faster than any other algorithms while sorting an array of numbers (integers) and binary strings.
