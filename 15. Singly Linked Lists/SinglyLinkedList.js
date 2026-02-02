417893d693c70bf3402085a1f074c0acac806269
sh4aadman <sh4adman@gmail.com>
2026-02-03 01:26:34 +0600

    03-02-26

---
+++ 15. Singly Linked Lists/SinglyLinkedList.js
@@ -0,0 +1,60 @@
+// piece of data - val
+// reference to the next node - next
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+// var first = new Node("Hi");
+// first.next = new Node("there");
+// console.log(first);
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    var current = this.head;
+    var newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = newTail.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+}
+
+var list = new SinglyLinkedList();
+list.push("Hello!");
+list.push("there!");
+list.push("How are you?");
+console.log(list);
+console.log(list.pop());
+console.log(list.pop());
