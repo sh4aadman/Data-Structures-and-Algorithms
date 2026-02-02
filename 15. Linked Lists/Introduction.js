bf5d02c1fdf92c746c03d144139885e33f22da96
sh4aadman <sh4adman@gmail.com>
2026-02-02 15:11:22 +0600

    02-02-26

---
+++ 15. Linked Lists/Introduction.js
@@ -0,0 +1,11 @@
+// Linked Lists - contains head, tail, length property.
+
+// Consists of nodes - each node has a value and a pointer to another node or null.
+
+// Comparison with Arrays - do not have indeces.
+
+// Connected via nodes with a next pointer.
+
+// Random access is not allowed.
+
+// Where arrays are indexed in order - insertion and deletion can be expensive - can quickly be accessed at a specific index.
