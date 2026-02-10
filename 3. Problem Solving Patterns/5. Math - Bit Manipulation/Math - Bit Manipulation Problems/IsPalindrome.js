4362393937dee1e3d62df3c364ce518ed1b6b0a8
sh4aadman <sh4adman@gmail.com>
2026-02-10 14:54:43 +0600

    10-02-26

---
+++ 3. Problem Solving Patterns/5. Math - Bit Manipulation/Math - Bit Manipulation Problems/IsPalindrome.js
@@ -0,0 +1,12 @@
+// Leetcode - Problem List - Problem 09
+
+function isPalindrome(x) {
+    
+ }
+
+console.log(isPalindrome(121));
+console.log(isPalindrome(-121));
+console.log(isPalindrome(10));
+
+// Time Complexity - O()
+// Space Complexity - O()
---
+++ 3. Problem Solving Patterns/5. Math - Bit Manipulation/Math_BitManipulation.js
@@ -0,0 +1,16 @@
+// Bit Manipulation uses bitwise operators - to perform operations directly on the individual bits (binary representations) of numbers.
+
+// Bitwise Operators
+// Bitwise AND - Returns 1 if both bits are one -> 5(101) & 1(001) - returns 1(001)
+// Bitwise OR - Returns 1 if at least one bit is 1, otherwise 0 -> 5(101) | 3(011) - returns 7(111)
+// Bitwise XOR - Returns 1 if the bits are different, otherwise 0 -> 5(101) ^ 1(001) - returns 4(100)
+// Bitwise NOT - Inverts all the bits -> ~5(000 101) - returns -6(111 010)
+// Left Shift - Shifts bits left by a specified number, pushing bits (zeroes) from right -> 5(101) << 1 - returns 10(1010)
+// Signed Right Shift - Shifts bits right, pushing copies of the leftmost sign bit in from the left -> 5(101) >> 1 - returns 2(10)
+// Zero-fill Right Shift - Shifts bits right, pushing zeroes in from the left, regardless of sign -> 5(101) >>> 1 - returns 2(10)
+
+// Common Techniques
+// Odd / Even Number -> num & 1 - returns true if the number is odd.
+// Swapping two numbers - 
+// Getting a specific bit - 
+// Setting a specific bit - 
