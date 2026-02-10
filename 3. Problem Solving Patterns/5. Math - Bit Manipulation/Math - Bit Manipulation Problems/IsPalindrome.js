674c473b8316bb816bf6ec8ec686a702a8a21074
sh4aadman <sh4adman@gmail.com>
2026-02-10 19:28:41 +0600

    10-02-26

--- 3. Problem Solving Patterns/5. Math - Bit Manipulation/Math - Bit Manipulation Problems/IsPalindrome.js
+++ 3. Problem Solving Patterns/5. Math - Bit Manipulation/Math - Bit Manipulation Problems/IsPalindrome.js
@@ -1,12 +1,27 @@
 // Leetcode - Problem List - Problem 09
 
 function isPalindrome(x) {
-    
- }
+  if (x < 0) return false;
+
+  var div = 1;
+  while (x >= 10 * div) div *= 10;
+
+  while (x) {
+    var left = Math.floor(x / div);
+    var right = x % 10;
+
+    if (left !== right) return false;
+
+    x = Math.floor((x % div) / 10);
+    div = div / 100;
+  }
+
+  return true;
+}
 
 console.log(isPalindrome(121));
 console.log(isPalindrome(-121));
 console.log(isPalindrome(10));
 
-// Time Complexity - O()
-// Space Complexity - O()
+// Time Complexity - O(logn)
+// Space Complexity - O(1)
