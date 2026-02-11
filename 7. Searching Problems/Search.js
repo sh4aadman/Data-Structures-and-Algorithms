27c5bdfcd8045ecddeebf3fb2539f435ea710372
sh4aadman <sh4adman@gmail.com>
2026-02-11 20:01:29 +0600

    11-02-26

---
+++ 7. Searching Problems/Search.js
@@ -0,0 +1,11 @@
+// Leetcode - Problem List - Problem 704
+
+function search(nums, target) {
+    
+}
+
+console.log(search([-1, 0, 3, 5, 9, 12], 9));
+console.log(search([-1, 0, 3, 5, 9, 12], 2));
+
+// Time Complexity - O()
+// Space Complexity - O()
