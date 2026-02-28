e0b546aea13a585df82bc18f5127b4c5dae89e9b
sh4aadman <sh4adman@gmail.com>
2026-02-28 16:37:03 +0600

    28-02-26

--- 20. Doubly Linked Lists/DoublyLinkedList.js
+++ 20. Doubly Linked Lists/DoublyLinkedList.js
@@ -34,20 +34,54 @@ class DoublyLinkedList {
     }
     this.length++;
     return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    var removedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = removedNode.prev;
+      this.tail.next = null;
+      removedNode.prev = null;
+    }
+    this.length--;
+    return removedNode;
+  }
+
+  shift() {
+    if (!this.head) return undefined;
+    var oldHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    this.length--;
+    return oldHead;
     }
     
-    pop() {
-        if (!this.head) return undefined;
-        var removedNode = this.tail;
-        if (this.length === 1) {
-            this.head = null;
-            this.tail = null;
+    unshift(val) {
+        var newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
         } else {
-            this.tail = removedNode.prev;
-            this.tail.next = null;
-            removedNode.prev = null;
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
         }
-        this.length--;
-        return removedNode;
+        this.length++;
+        return this;
     }
 }
+
+var list = new DoublyLinkedList();
+list.push("Harry");
+list.push("Ron");
+list.push("Hermione");
