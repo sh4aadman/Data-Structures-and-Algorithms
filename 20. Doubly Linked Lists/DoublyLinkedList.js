ef5fa6ed3ea7f6bb95e6484a8f836be1438ea69a
sh4aadman <sh4adman@gmail.com>
2026-02-28 15:43:37 +0600

    28-02-26

--- 20. Doubly Linked Lists/DoublyLinkedList.js
+++ 20. Doubly Linked Lists/DoublyLinkedList.js
@@ -3,11 +3,11 @@
 // reference to the previous node - prev
 
 class Node {
-    constructor(val) {
-        this.val = val;
-        this.next = null;
-        this.prev = null;
-    }
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
 }
 
 // var first = new Node("Hi");
@@ -16,9 +16,38 @@ class Node {
 // console.log(first);
 
 class DoublyLinkedList {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+    }
+    
+    pop() {
+        if (!this.head) return undefined;
+        var removedNode = this.tail;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = removedNode.prev;
+            this.tail.next = null;
+            removedNode.prev = null;
+        }
+        this.length--;
+        return removedNode;
     }
 }
