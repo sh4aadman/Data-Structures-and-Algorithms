91bdb7407ea7c0cd6d9c244fbe0791a5636964d4
sh4aadman <sh4adman@gmail.com>
2026-03-01 13:01:32 +0600

    01-03-26

--- 20. Doubly Linked Lists/DoublyLinkedList.js
+++ 20. Doubly Linked Lists/DoublyLinkedList.js
@@ -109,6 +109,36 @@ class DoublyLinkedList {
     }
     return false;
   }
+
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === 0) return !!this.unshift(val);
+    if (index === this.length) return !!this.push(val);
+
+    var newNode = new Node(val);
+    var beforeNode = this.get(index - 1);
+    var afterNode = beforeNode.next;
+
+    ((beforeNode.next = newNode), (newNode.prev = beforeNode));
+    ((newNode.next = afterNode), (afterNode.prev = newNode));
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    
+    var removedNode = this.get(index);
+    var beforeNode = removedNode.prev;
+    var afterNode = removedNode.next;
+    
+    ((beforeNode.next = afterNode), (afterNode.prev = beforeNode));
+    ((removedNode.next = null), (removedNode.prev = null));
+    this.length--;
+    return removedNode;
+  }
 }
 
 var list = new DoublyLinkedList();
