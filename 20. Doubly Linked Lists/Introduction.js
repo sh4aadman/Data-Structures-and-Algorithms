0edd3620cea79cce1de8af2e3ec8ecffd1c90e82
sh4aadman <sh4adman@gmail.com>
2026-03-01 14:52:23 +0600

    01-03-26

--- 20. Doubly Linked Lists/Introduction.js
+++ 20. Doubly Linked Lists/Introduction.js
@@ -8,6 +8,8 @@
 
 // Insertion and Deletion both at the begining and the end of the list are of constant time O(1).
 
+// However, they do take up more memory considering the extra pointer.
+
 // Doubly Linked Lists are an excellent choice for - browser history (back and forward), music playlists (skipping to the next/previous song), undo/redo in text editors.
 
 // The idea of a list data structure that consists of nodes - Stacks and Queues.
