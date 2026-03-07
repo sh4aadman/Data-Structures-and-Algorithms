7fb4e15997f89f7a08222e80e8b76d00072d8652
sh4aadman <sh4adman@gmail.com>
2026-03-07 19:53:46 +0600

    07-03-26

--- 23. Stacks and Queues Problems/01. Stacks Problems/RemoveStars.js
+++ 23. Stacks and Queues Problems/01. Stacks Problems/RemoveStars.js
@@ -1,5 +1,7 @@
 // Leetcode - Problem List - Problem 2390
 
+// Intuition - * will only affect the characters before it. The input s works like tapping and * works like the deletion action.
+
 function removeStars(s) {
   var stack = [];
 
@@ -19,4 +21,19 @@ console.log(removeStars("erase*****"));
 console.log(removeStars("*"));
 
 // Time Complexitty - O(n)
-// Space Complexitty - O(n)
+// Space Complexitty - O(n) [stack] / O(1) [without-stack]
+
+// Two Pointer Approach
+function removeStars(s) {
+  var arr = s.split("");
+  var write = 0;
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "*") {
+      write--;
+    } else {
+      arr[write++] = arr[i];
+    }
+  }
+  return arr.slice(0, write).join("");
+}
