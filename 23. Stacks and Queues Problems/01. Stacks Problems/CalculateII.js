4a947fbac77e24e47df7f3b0ec9c7751739e5317
sh4aadman <sh4adman@gmail.com>
2026-03-07 16:58:47 +0600

    07-03-26

--- 23. Stacks and Queues Problems/01. Stacks Problems/CalculateII.js
+++ 23. Stacks and Queues Problems/01. Stacks Problems/CalculateII.js
@@ -44,5 +44,35 @@ console.log(calculate("3+2*2"));
 console.log(calculate(" 3/2 "));
 console.log(calculate(" 3+5 / 2 "));
 
-// Time Complexittyt - O(n)
-// Space Complexittyt - O(n) [stack] / O(1) [without-stack]
+// Time Complexity - O(n)
+// Space Complexity - O(n) [stack] / O(1) [without-stack]
+
+function calculate(s) {
+  var currentNumber = 0;
+  var lastNumber = 0;
+  var result = 0;
+  var operation = "+";
+
+  for (var i = 0; i < s.length; i++) {
+    var char = s[i];
+
+    if (char >= "0" && char <= "9") {
+      currentNumber = currentNumber * 10 + (char - "0");
+    }
+
+    if ((isNaN(char) && char !== " ") || i === s.length - 1) {
+      if (operation === "+" || operation === "-") {
+        result += lastNumber;
+        lastNumber = operation === "+" ? currentNumber : -currentNumber;
+      } else if (operation === "*") {
+        lastNumber = lastNumber * currentNumber;
+      } else if (operation === "/") {
+        lastNumber = Math.trunc(lastNumber / currentNumber);
+      }
+      operation = char;
+      currentNumber = 0;
+    }
+  }
+  result += lastNumber;
+  return result;
+}
