5e7dae22abfe23f2a72ef450f6115fb099562126
sh4aadman <sh4adman@gmail.com>
2026-03-10 12:20:59 +0600

    10-03-26

---
+++ 23. Stacks and Queues Problems/02. Queues Problems/PredictPartyVictory.js
@@ -0,0 +1,36 @@
+// Leetcode - Problem List - Problem 649
+
+// Intuition - To play optimally, a senator should always ban the next available opponent, By banning the opponent who would have voted soonest, the senator prevents an immediate threat to their own party, Since the voting is round-based and circular, the most efficient way to solve this is by using two Queues to track the indices of the senators.
+
+function predictPartyVictory(senate) {
+    var n = senate.length;
+    var radiant = [];
+    var dire = [];
+
+    for (var i = 0; i < n; i++) {
+        if (senate[i] === "R") {
+            radiant.push(i);
+        } else {
+            dire.push(i);
+        }
+    }
+
+    while (radiant.length > 0 && dire.length > 0) {
+        var radiantVoter = radiant.shift();
+        var direVoter = dire.shift();
+
+        if (radiantVoter < direVoter) {
+            radiant.push(radiantVoter + n);
+        } else {
+            dire.push(direVoter + n);
+        }
+    }
+
+    return radiant.length > 0 ? "Radiant" : "Dire";
+}
+
+console.log(predictPartyVictory("RD"));
+console.log(predictPartyVictory("RDD"));
+
+// Time Complexity - O(n)
+// Space Complexity - O(n)
