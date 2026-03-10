134181c85f124c6f852a397f4c7b7490da94df69
sh4aadman <sh4adman@gmail.com>
2026-03-10 12:41:34 +0600

    10-03-26

--- 23. Stacks and Queues Problems/02. Queues Problems/PredictPartyVictory.js
+++ 23. Stacks and Queues Problems/02. Queues Problems/PredictPartyVictory.js
@@ -3,30 +3,30 @@
 // Intuition - To play optimally, a senator should always ban the next available opponent, By banning the opponent who would have voted soonest, the senator prevents an immediate threat to their own party, Since the voting is round-based and circular, the most efficient way to solve this is by using two Queues to track the indices of the senators.
 
 function predictPartyVictory(senate) {
-    var n = senate.length;
-    var radiant = [];
-    var dire = [];
+  var n = senate.length;
+  var radiant = [];
+  var dire = [];
 
-    for (var i = 0; i < n; i++) {
-        if (senate[i] === "R") {
-            radiant.push(i);
-        } else {
-            dire.push(i);
-        }
+  for (var i = 0; i < n; i++) {
+    if (senate[i] === "R") {
+      radiant.push(i);
+    } else {
+      dire.push(i);
     }
+  }
 
-    while (radiant.length > 0 && dire.length > 0) {
-        var radiantVoter = radiant.shift();
-        var direVoter = dire.shift();
+  while (radiant.length > 0 && dire.length > 0) {
+    var radiantVoter = radiant.shift();
+    var direVoter = dire.shift();
 
-        if (radiantVoter < direVoter) {
-            radiant.push(radiantVoter + n);
-        } else {
-            dire.push(direVoter + n);
-        }
+    if (radiantVoter < direVoter) {
+      radiant.push(radiantVoter + n);
+    } else {
+      dire.push(direVoter + n);
     }
+  }
 
-    return radiant.length > 0 ? "Radiant" : "Dire";
+  return radiant.length > 0 ? "Radiant" : "Dire";
 }
 
 console.log(predictPartyVictory("RD"));
@@ -34,3 +34,44 @@ console.log(predictPartyVictory("RDD"));
 
 // Time Complexity - O(n)
 // Space Complexity - O(n)
+
+// Opttimized Ban Debt Approach
+function predictPartyVictory(senate) {
+  var queue = senate.split("");
+  var radiantBanDebt = 0;
+  var direBanDebt = 0;
+
+  var radiantCount = 0;
+  var direCount = 0;
+  for (var char of queue) {
+    if (char === "R") {
+      radiantCount++;
+    } else {
+      direCount++;
+    }
+  }
+
+  while (radiantCount > 0 && direCount > 0) {
+    var current = queue.shift();
+
+    if (current === "R") {
+      if (radiantBanDebt > 0) {
+        radiantBanDebt--;
+        radiantCount--;
+      } else {
+        direBanDebt++;
+        queue.push("R");
+      }
+    } else {
+      if (direBanDebt > 0) {
+        direBanDebt--;
+        direCount--;
+      } else {
+        radiantBanDebt++;
+        queue.push("D");
+      }
+    }
+  }
+
+  return radiantCount > 0 ? "Radiant" : "Dire";
+}
